940f0a0fb614f836def06d9174ca6c7525becf0e
Christopher S. Case <chris.case@g33xnexus.com>
2013-11-27 11:19:56 -0600

    removed unneeded example file.

--- examples/mp3player.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var lame = require('lame');
-var Speaker = require('speaker');
-
-fs.createReadStream(process.argv[2])
-    .pipe(new lame.Decoder())
-    .on('format', function (format) {
-        this.pipe(new Speaker(format));
-    });

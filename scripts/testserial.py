2882c40696c638e6d2e42d3c14db1536b062022e
David H. Bronke <dbronke@armt.com>
2013-12-10 09:43:12 -0600

    Made testserial.py slightly more portable and robust, added docstring and shebang, and cleaned up output.

--- scripts/testserial.py
+++ scripts/testserial.py
@@ -1,23 +1,35 @@
+#!/usr/bin/env python
+"""Test the LSDG lights app, emulating the Raspberry Pi light controller.
+
+This script opens a pseudo-TTY to emulate a serial port connected to the RPi, and then displays light states in a
+vaguely "graphical" fashion on the console.
+
+"""
+from __future__ import print_function
 import os
+import pty
 import re
 import subprocess
 import sys
 import time
+import traceback
 
 
 lightCount = 32
 
 lightCmdRE = re.compile(r'p(\d+)s([01])')
 
-master, slave = os.openpty()
 
-print(os.ttyname(master))
-print(os.ttyname(slave))
+# Open a pseudo-TTY to emulate a serial port.
+master, slave = pty.openpty()
 
 env = dict(os.environ)
 env['LSDG_LIGHTS_SERIAL'] = os.ttyname(slave)
 
+
+print("Running 'npm start' with LSDG_LIGHTS_SERIAL={!r}...".format(env['LSDG_LIGHTS_SERIAL']))
 proc = subprocess.Popen(["npm", "start"], env=env)
+
 
 try:
     lightStates = [False] * lightCount
@@ -44,7 +56,14 @@ try:
                 for match in lightCmdRE.finditer(line):
                     channel, state = map(int, match.group(1, 2))
 
-                    lightStates[channel] = bool(state)
+                    try:
+                        lightStates[channel] = bool(state)
+                    except KeyError:
+                        print('\033[1;31mERROR:\033[0;91m Invalid channel number {!r}! (must be between 0 and {})'
+                                .format(channel, lightCount - 1))
+                    except:
+                        print('\033[1;31mERROR:\033[0;91m Exception while setting light states!\n{}\033[m'
+                                .format(traceback.format_exc()))
 
                 showLightStates()
 
